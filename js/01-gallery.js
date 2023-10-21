c19164aeecbfb7a24528bc2a6196b6900987ca40
Valera <104449560+Valera024@users.noreply.github.com>
2023-10-21 14:04:53 +0300

    2.0

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,33 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const list = document.querySelector(".gallery");
 
-galleryItems.forEach(({ preview, original, description }) => {
-    const li = document.createElement("li");
-    const link = document.createElement("a");
-    const img = document.createElement("img");
-    li.classList.add("gallery__item");
-    link.classList.add("gallery__link");
-    img.classList.add("gallery__image");
-    img.src = preview;
-    img.alt = description;
-    img.dataset.source = original;
-    link.href = original;
-    link.appendChild(img);
-    li.appendChild(link);
-    list.appendChild(li);
-});
+const listEl = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+  </a>
+</li>`
+).join("");
+
+list.insertAdjacentHTML("afterbegin", listEl);
 
 console.log(galleryItems);
+
+function keydownHandler(event, modalImg) {
+    if (event.key === "Escape") {
+        modalImg.close();
+    }
+}
 
 list.addEventListener("click", (event) => {
     event.preventDefault();
     const originalImg = event.target.dataset.source;
     
-    const instance = basicLightbox.create(`<img src="${originalImg}">`);
-    instance.show();
-    window.addEventListener("keydown", (event) => {
-        if (event.key === "Escape") {
-            instance.close()
+    const modalImg = basicLightbox.create(`<img src="${originalImg}">`, {     
+        onShow: (modalImg) => {
+            window.addEventListener("keydown", (event) => keydownHandler(event, modalImg))
+        },
+        onClose: (modalImg) => {
+            window.removeEventListener("keydown", (event) => keydownHandler(event, modalImg))
         }
-    })
+    });
+    modalImg.show();
 })
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,20 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const list = document.querySelector(".gallery");
 
-galleryItems.forEach(({ preview, original, description }) => {
-    const li = document.createElement("li");
-    const link = document.createElement("a");
-    const img = document.createElement("img");
-    li.classList.add("gallery__item");
-    link.classList.add("gallery__link");
-    img.classList.add("gallery__image");
-    link.href = original;
-    img.src = preview;
-    img.alt = description;
-    link.appendChild(img);
-    li.appendChild(link);
-    list.appendChild(li);
-})
+const listEl = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+).join("");
+
+list.insertAdjacentHTML("afterbegin", listEl);
+
 console.log(galleryItems);
 
 let gallery = new SimpleLightbox('.gallery a', {
